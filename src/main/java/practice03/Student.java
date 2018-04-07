29c81684b4e187ec854a49ed4bcecb1ade9c59f1
ctc <1171332103@qq.com>
2018-04-07 15:44:00 +0800

    提交练习3，通过所有测试

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,36 @@
 package practice03;
 
-public class Student {
+public class Student extends Person {
+    private int age;
+    private String name;
+    private int klass;
+
+    public Student(String name, int age) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public Student(String name, int age, int klass){
+        this.age = age;
+        this.name = name;
+        this.klass = klass;
+    }
+    @Override
+    public String introduce() {
+        return "I am a Student. I am at Class "+klass+".";
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
 }
--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,27 @@
 package practice03;
 
 public class Worker {
+    private int age;
+    private String name;
+
+    public Worker(String name, int age) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public Worker() {
+    }
+
+    public String introduce(){
+        return "I am a Worker. I have a job.";
+    }
+
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
