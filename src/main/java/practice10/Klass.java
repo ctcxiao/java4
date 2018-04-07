51bb0f768affb15776dc5d081095dae68cb0a39a
ctc <1171332103@qq.com>
2018-04-07 19:42:16 +0800

    提交练习10，通过所有测试

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,43 @@
 package practice10;
 
 public class Klass {
+    private int number;
+    private Student student;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return "Class " + number;
+    }
+
+    public void assignLeader(Student student) {
+        if (!student.isInClass()) {
+            System.out.print("It is not one of us.\n");
+            return;
+        }
+        student.setLeader(true);
+        this.student = student;
+    }
+
+    public Student getLeader() {
+        return student;
+    }
+
+    public void appendMember(Student student) {
+        student.setInClass(true);
+    }
+
+    public boolean isStudentInKlass(Student student) {
+        return student.getKlass().number == number;
+    }
 }
