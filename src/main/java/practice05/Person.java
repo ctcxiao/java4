3afa036c0cc6cfdbd9bb7ee1633c7d0cd2353d1b
ctc <1171332103@qq.com>
2018-04-07 15:56:40 +0800

    提交练习5，通过所有测试

--- src/main/java/practice05/Person.java
+++ src/main/java/practice05/Person.java
@@ -1,4 +1,26 @@
 package practice05;
 
 public class Person {
+    private int age;
+    private String name;
+
+    public Person(String name, int age) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public Person() {
+    }
+
+    public String introduce(){
+        return "My name is Tom. I am 21 years old.";
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,37 @@
 package practice05;
 
-public class Student {
+public class Student extends Person {
+    private int age;
+    private String name;
+    private int klass;
+
+    public Student(String name, int age) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public Student(String name, int age, int klass) {
+        this.age = age;
+        this.name = name;
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a Student. I am at Class 2.";
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
 }
