ee4eee3796613297e3929a3ed80dcfea3ccb0d0d
ctc <1171332103@qq.com>
2018-04-07 15:30:20 +0800

    提交Person类，测试通过

--- src/main/java/practice01/Person.java
+++ src/main/java/practice01/Person.java
@@ -1,4 +1,25 @@
 package practice01;
 
+import java.util.stream.Stream;
+
 public class Person {
+    private int age;
+    private String name;
+
+    public String introduce(){
+        return "My name is Tom. I am 21 years old.";
+    }
+    
+    public Person(String name, int age) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
